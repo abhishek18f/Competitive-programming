bb292ab81cc1cc9c37ed1fc4dc7e3d63fa7b6089
Abhishek Kumar <abhishek18f@iitg.ac.in>
2020-07-16 05:11:44 +0530

    leetcode 875

---
+++ LeetCode/BinarySearch_875.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int l = 1 , u = 2*1e9;
+        
+        while(l<u)
+        {
+            int req = 0;
+            int mid = (l+u)/2;
+            for(int i = 0 ; i< piles.size() ; i++)
+            {
+                req += ((piles[i] - 1)/mid + 1);
+                //req += ceil(piles[i]/mid);
+            }
+            
+            if(req<=H) u = mid;
+            else l= mid+1;
+        }
+        return l;
+    }
+};
--- template.cpp
+++ template.cpp
@@ -37,5 +37,6 @@ int main()
     while(t--)
     {
         solve();
+        
     }
 }
